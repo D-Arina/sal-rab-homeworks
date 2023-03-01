e6a2495e6249f6ec74117211aabb35644fe2f6c8
Arina <eparis@atlassian.com>
2023-03-01 14:53:00 +0500

    Task 4 done

--- task_4/solution.js
+++ task_4/solution.js
@@ -6,9 +6,21 @@ function parseProducts(json) {
     // Преобразуйте строку json, переданную как аргумент функции,
     // в объект с помощью функции JSON.parse(json)
     // и запишите в переменную data
+    let data = JSON.parse(json);
     // Верните как результат функции свойство products объекта data
+    return data.products;
 }
 
+function renderProductsCards(json) {
+    clearProducts;
+    let products = parseProducts(json);
+    length = products.length;
+
+
+    for (let i = 0; i < length; i +=1) {
+        addProduct(products[i]);
+        }
+}
 // Напишите функцию renderProductsCards(json)
 // Аргументом функции является JSON
 // Выполните функцию clearProducts - она уже написана в коде, просто вызовите ее, аргументы не требуются
